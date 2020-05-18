be77450320bc571fca85d833c2ccac363acd9918
James Smith <jamesplease2@gmail.com>
2020-05-17 23:08:47 -0700

    Add support for KaTeX

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,21 @@
+const mk = require('@traptitech/markdown-it-katex');
+
 module.exports = function (config) {
+  let markdownIt = require("markdown-it");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+
+  const md = markdownIt(options);
+  md.use(mk, {"blockClass": "math-block", "errorColor" : " #cc0000"})
+
+  config.setLibrary("md",md );
+
   config.addLayoutAlias('default', 'layouts/page-layout.njk');
   config.addPassthroughCopy({ 'src/public': "." });
+  config.addPassthroughCopy({ "src/fonts": "css" });
+  config.addPassthroughCopy({ "src/css/katex.*.css": "css" });
   config.addTransform("htmlmin", require("./utils/minify-html.js"));
 };

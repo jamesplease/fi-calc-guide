811eeef3b72e8c80b5a965a74860e0d8f7b2bfd6
James Smith <jamesplease2@gmail.com>
2020-05-18 00:18:53 -0700

    Start work on nav

---
+++ src/_data/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    title: "Introduction",
+    url: "/about/",
+    pages: [
+      {
+        title: "Getting Started",
+        url: "/",
+      },
+      {
+        title: "Backtesting",
+        url: "/contact/",
+      },
+      {
+        title: "Limitations",
+        url: "/limitations/",
+      },
+    ],
+  },
+];

86f169113597aa604bffb6cf938cb23a7b9ef1c0
James Please <james_please@icloud.com>
2021-05-01 16:49:38 -0700

    Refactor

--- src/_includes/js/color-mode.js
+++ src/_includes/js/color-mode.js
@@ -3,8 +3,3 @@ const isLightMode =
   window.matchMedia('(prefers-color-scheme: light)').matches;
 const bodyClass = isLightMode ? 'lightMode' : 'darkMode';
 document.body.classList.add(bodyClass);
-
-// const logo = document.querySelector('.home_titleImg');
-// if (logo && isLightMode) {
-//   logo.src = '/fi-calc-logo-light.png';
-// }
